2eb7f6532f23343015f0a83ed24ad9bab87b60a6
Tiger Chen <Tiger@Tigers-MacBook-Pro.local>
2018-06-02 10:09:01 -0500

    Layout for Login

--- app/src/main/java/org/danielsoares/pickupapp/Login.java
+++ app/src/main/java/org/danielsoares/pickupapp/Login.java
@@ -3,11 +3,16 @@ package org.danielsoares.pickupapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.Context;
 import android.view.View;
 
 public class Login extends AppCompatActivity {
 
     private Button loginButton;
+    private EditText email;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +20,37 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         loginButton = (Button) findViewById(R.id.loginButton);
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+
+        final String emailString = email.getText().toString();
+        final String passwordString = password.getText().toString();
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Log in. Go to another activity and sends info to server
+                    if (emailString == null) {
+
+                        Context context = getApplicationContext();
+                        CharSequence text = "Please Enter Email First";
+                        int duration = Toast.LENGTH_LONG;
+
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                        return;
+                    }
+                    if (passwordString == null) {
+
+                        Context context = getApplicationContext();
+                        CharSequence text = "Please Enter Password First";
+                        int duration = Toast.LENGTH_LONG;
+
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                        return;
+                    }
             }
+                // Log in. Go to another activity and sends info to server
         });
+
     }
 }
