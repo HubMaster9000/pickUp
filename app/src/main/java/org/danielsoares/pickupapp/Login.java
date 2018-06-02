9c439e285496ae6854345acf444dd8814eea642c
Tiger Chen <Tiger@Tigers-MacBook-Pro.local>
2018-06-02 09:47:54 -0500

    penis

--- app/src/main/java/org/danielsoares/pickupapp/Login.java
+++ app/src/main/java/org/danielsoares/pickupapp/Login.java
@@ -2,12 +2,23 @@ package org.danielsoares.pickupapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
+
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Log in. Go to another activity and sends info to server
+            }
+        });
     }
 }
