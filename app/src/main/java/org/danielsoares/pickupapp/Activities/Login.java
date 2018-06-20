d23eb93b114ea197f8ab108006bd766068bcfd46
eltiggre <tigerchen00@gmail.com>
2018-06-19 19:47:09 -0500

    Google login

--- app/src/main/java/org/danielsoares/pickupapp/Activities/Login.java
+++ app/src/main/java/org/danielsoares/pickupapp/Activities/Login.java
@@ -11,11 +11,13 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
 
+import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInApi;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -84,6 +86,8 @@ public class Login extends AppCompatActivity implements View.OnClickListener,   
         // [END configure_signin]
         // Build a GoogleSignInClient with the options specified by gso.
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        mGoogleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this, this).addApi(Auth.GOOGLE_SIGN_IN_API, gso).build();
+
 
         // [START customize_button]
         // Set the dimensions of the sign-in button.
@@ -92,6 +96,17 @@ public class Login extends AppCompatActivity implements View.OnClickListener,   
         signInButton.setColorScheme(SignInButton.COLOR_LIGHT);
         // [END customize_button]
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // [START on_start_sign_in]
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
+        // [END on_start_sign_in]
     }
 
     /**
@@ -134,31 +149,31 @@ public class Login extends AppCompatActivity implements View.OnClickListener,   
 
     }
 
-        /**
-         * Listens the click on view
-         *
-         * @param v
-         */
-        @Override
-        public void onClick(View v) {
-            switch (v.getId()) {
-                // Login
-                case R.id.appCompatButtonLogin:
-                    verifyFromSQLite();
-                    break;
-                // Directs you to Sign Up page
-                case R.id.textViewLinkRegister:
-                    // Navigate to RegisterActivity
-                    Intent intentRegister = new Intent(getApplicationContext(), SignUp.class);
-                    startActivity(intentRegister);
-                    break;
-                // Login with Google
-                case R.id.googleLoginButton:
-                    signIn();
-                    break;
+    /**
+     * Listens the click on view
+     *
+     * @param v
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            // Login
+            case R.id.appCompatButtonLogin:
+                verifyFromSQLite();
+                break;
+            // Directs you to Sign Up page
+            case R.id.textViewLinkRegister:
+                // Navigate to RegisterActivity
+                Intent intentRegister = new Intent(getApplicationContext(), SignUp.class);
+                startActivity(intentRegister);
+                break;
+            // Login with Google
+            case R.id.googleLoginButton:
+                signIn();
+                break;
 
-            }
         }
+    }
 
 
     /**
@@ -199,77 +214,37 @@ public class Login extends AppCompatActivity implements View.OnClickListener,   
         textInputEditTextPassword.setText(null);
     }
 
-    // [START onActivityResult]
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
+    private void handleResult(GoogleSignInResult result) {
+        if (result.isSuccess()) {
+            GoogleSignInAccount account = result.getSignInAccount();
+            String name = account.getDisplayName();
+            String email = account.getEmail();
+            String img_URL = account.getPhotoUrl().toString();
+            updateUI(true) ;
+        }
+        else {
+            updateUI(false);
         }
     }
-    // [END onActivityResult]
-
-    // [START handleSignInResult]
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-
-            // Signed in successfully, show authenticated UI.
-            updateUI(account);
-        } catch (ApiException e) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
-            updateUI(null);
-        }
-    }
-    // [END handleSignInResult]
 
     // [START signIn]
     private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
     // [END signIn]
 
+    private void updateUI(boolean isLogin) {
+
+    }
+
     @Override
-    protected void onStart() {
-        super.onStart();
-        // [START on_start_sign_in]
-        // Check for existing Google Sign In account, if the user is already signed in
-        // the GoogleSignInAccount will be non-null.
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        updateUI(account);
-        // [END on_start_sign_in]
-    }
-
-
-
-    // [START revokeAccess]
-    private void revokeAccess() {
-        mGoogleSignInClient.revokeAccess()
-                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        // [START_EXCLUDE]
-                        updateUI(null);
-                        // [END_EXCLUDE]
-                    }
-                });
-    }
-    // [END revokeAccess]
-
-    private void updateUI(@Nullable GoogleSignInAccount account) {
-        if (account != null) {
-            googleLoginButton.setVisibility(View.GONE);
-        } else {
-            googleLoginButton.setVisibility(View.VISIBLE);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleResult(result);
         }
-    }
 
+    }
 }
