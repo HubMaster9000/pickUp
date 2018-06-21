ba1506b018682361ee173e54bdc8ebef3005d102
eltiggre <tigerchen00@gmail.com>
2018-06-21 09:28:40 -0500

    working on start time

--- app/src/main/java/org/danielsoares/pickupapp/Activities/Make_A_New_Game.java
+++ app/src/main/java/org/danielsoares/pickupapp/Activities/Make_A_New_Game.java
@@ -1,7 +1,9 @@
 package org.danielsoares.pickupapp.Activities;
 
+import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import org.danielsoares.pickupapp.R;
 
@@ -11,5 +13,11 @@ public class Make_A_New_Game extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make__a__new__game);
+    }
+
+    // When button is pressed to select a time
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
     }
 }
